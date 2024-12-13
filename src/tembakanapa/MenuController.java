32a6928d5da9ce5e84984bf568287415d38ae401
wahyu wsap <152713472+wahyuwsap@users.noreply.github.com>
2024-12-13 21:54:01 +0700

    set mouse klik

--- src/tembakanapa/MenuController.java
+++ src/tembakanapa/MenuController.java
@@ -10,3 +10,17 @@ import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+public class MenuController implements Initializable {
+
+    @FXML
+    private ImageView playBtn;
+    @FXML
+    private ImageView quitBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Tambahkan aksi untuk ImageView sebagai tombol
+        quitBtn.setOnMouseClicked(event -> quitApplication());
+    }
+}
