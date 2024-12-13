b2999c294743459635187dc6adf3e2871a9f6746
wahyu wsap <152713472+wahyuwsap@users.noreply.github.com>
2024-12-13 21:30:46 +0700

    Create MenuController.java
    import library

---
+++ src/tembakanapa/MenuController.java
@@ -0,0 +1,8 @@
+package tembakanapa;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
